5d57262cfe6124522fae22a97e50968b6891568b
Adwiti789 <adwiti1975@gmail.com>
2018-07-31 23:16:19 -0700

    initial commit

---
+++ src/main/java/com/arun/rc/SpringRestCommonApplication.java
@@ -0,0 +1,12 @@
+package com.arun.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCommonApplication.class, args);
+	}
+}
